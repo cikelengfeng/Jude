f7959a3915c66c7654520e7fd7c638ada08d5c0a
DongXu <cikelengfeng@gmail.com>
2017-02-17 20:15:01 +0800

    Initial Commit

---
+++ Jude/Jude.h
@@ -0,0 +1,19 @@
+//
+//  Jude.h
+//  Jude
+//
+//  Created by 徐 东 on 2017/2/17.
+//  Copyright © 2017年 dx lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jude.
+FOUNDATION_EXPORT double JudeVersionNumber;
+
+//! Project version string for Jude.
+FOUNDATION_EXPORT const unsigned char JudeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jude/PublicHeader.h>
+
+
